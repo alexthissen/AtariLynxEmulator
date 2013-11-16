e7cd59418d6f1b616aa06638adb478b43e200c45
alexthissen_cp <SND\alexthissen_cp>
2013-11-16 08:49:29 +0000

    Introduced new clients for Windows, Linux and Windows Phone 8 based on MonoGame 3.0

---
+++ Source/AtariLynxEmulator/Clients/MonoGame/EmulatorClient.Linux/Program.cs
@@ -0,0 +1,27 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using KillerApps.Gaming.Atari.MonoGame;
+#endregion
+
+namespace EmulatorClient.Linux
+{
+#if WINDOWS || LINUX
+	/// <summary>
+	/// The main class.
+	/// </summary>
+	public static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			using (var game = new EmulatorClient())
+				game.Run();
+		}
+	}
+#endif
+}
