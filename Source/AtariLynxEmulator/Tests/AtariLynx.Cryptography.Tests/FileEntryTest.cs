f14dc5136ecc6c47144d70aae3acbdd45bde7735
alexthissen_cp <SND\alexthissen_cp>
2014-07-10 20:54:57 +0000

    Some additional missing source files. APB and Quadromania files not included on purpose. Will fix with different binary files.

---
+++ Source/AtariLynxEmulator/Tests/AtariLynx.Cryptography.Tests/FileEntryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KillerApps.Emulation.Atari.Lynx.Cryptography;
+
+namespace AtariLynx.Encryption.Tests
+{
+	[TestClass]
+	public class FileEntryTest
+	{
+		private byte[] romImage = new byte[] {
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xE2, 0x01, 0x00, 0x00, 0x24,
+      0xF1, 0x09, 0x05, 0xD3, 0x01, 0x00, 0x00, 0x08, 0xCC, 0x8F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+		};
+		private byte[] singleFileEntry = new byte[] {
+			0x02, 0xE2, 0x01, 0x88, 0x00, 0x24, 0xF1, 0x09
+		};
+
+		[TestMethod]
+		public void DeserializeSingleFileEntryShouldGiveCorrectProperties()
+		{
+//			FileStream romImageStream = new FileStream(
+	//			Path.Combine(TestContext.TestDeploymentDir, RomImageFilePath), FileMode.Open, FileAccess.Read);
+			FileEntry entry = FileEntry.FromByteArray(singleFileEntry);
+
+			Assert.AreEqual<byte>(0x02, entry.PageOffset, "Page offset for file not correct.");
+			Assert.AreEqual<ushort>(0x01E2, entry.ByteOffset, "Byte offset for file entry not correct.");
+			Assert.AreEqual<byte>(0x88, entry.Flag, "Flag for file entry not correct.");
+			Assert.AreEqual<ushort>(0x2400, entry.RamDestination, "Ram destination for file not correct.");
+			Assert.AreEqual<ushort>(0x09F1, entry.FileSize, "File size for file not correct.");
+		}
+	}
+}
---
+++ Source/AtariLynxEmulator/Tests/AtariLynx.Cryptography.Tests/SboxHashAlgorithmTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using KillerApps.Emulation.Atari.Lynx.Cryptography;
+
+namespace AtariLynx.Encryption.Tests
+{
+	[TestClass]
+	public class SboxHashAlgorithmTest
+	{
+		private TestContext testContextInstance;
+		public const string RomImage128KFilePath = @"Quadromania.bin";
+		public const string RomImage256KFilePath = @"APB.bin";
+		private const int HeaderOffset = 410; 
+		
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext
+		{
+			get { return testContextInstance; }
+			set { testContextInstance = value; }
+		}
+
+		private readonly byte[] QuadroManiaHashValue = new byte[] 
+		{ 
+			0xAF, 0xC0, 0x08, 0x1E, 0xFE, 0x47, 0x4F, 0x37, 
+			0xE1, 0xA2, 0xE0, 0x50, 0x34, 0x02, 0x8A, 0x24 
+		};
+
+		private readonly byte[] ApbHashValue = new byte[]
+		{
+			0x65, 0x39, 0x1C, 0xC5, 0x6C, 0x6D, 0x55, 0x6C, 
+			0xCB, 0xA3, 0x41, 0xBB, 0xF1, 0xD8, 0xC5, 0x7C
+		};
+
+		[DeploymentItem(@"Binaries\" + RomImage128KFilePath)]
+		[TestMethod]
+		public void ComputeHashFor128KImageShouldReturnCorrectValue()
+		{
+			FileStream romImageStream = new FileStream(
+				Path.Combine(TestContext.TestDeploymentDir, RomImage128KFilePath), FileMode.Open, FileAccess.Read);
+			int romLength = (int)romImageStream.Length;
+			byte[] image = new byte[romLength];
+			int bytesRead	= romImageStream.Read(image, 0, romLength);
+
+			SboxHashAlgorithm algorithm = SboxHashAlgorithm.Create(romLength);
+			byte[] hash = algorithm.ComputeHash(image, 0, romLength);
+
+			Assert.AreEqual<int>(romLength, bytesRead, "Data read is not same length as expected length");
+			CollectionAssert.AreEqual(QuadroManiaHashValue, hash, "Computed hash is not correct.");
+		}
+
+		[DeploymentItem(@"Binaries\" + RomImage256KFilePath)]
+		[TestMethod]
+		public void ComputeHashFor256KImageShouldReturnCorrectValue()
+		{
+			FileStream romImageStream = new FileStream(
+				Path.Combine(TestContext.TestDeploymentDir, RomImage256KFilePath), FileMode.Open, FileAccess.Read);
+			int romLength = (int)romImageStream.Length;
+			byte[] image = new byte[romLength];
+			int bytesRead = romImageStream.Read(image, 0, romLength);
+
+			SboxHashAlgorithm algorithm = SboxHashAlgorithm.Create(romLength);
+			byte[] hash = algorithm.ComputeHash(image, 0, romLength);
+
+			Assert.AreEqual<int>(romLength, bytesRead, "Data read is not same length as expected length");
+			CollectionAssert.AreEqual(ApbHashValue, hash, "Computed hash is not correct.");
+		}
+	}
+}
