7c186c4ab1e7e926f94df7ce429349b04f87b423
Alex Thissen <athissen@xpirit.com>
2020-10-06 20:25:56 +0200

    Core 8-Bit emulation library

---
+++ src/Core/KillerApps.Emulation.Core8Bit/IMemoryAccessT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KillerApps.Emulation.Core
+{
+	public interface IMemoryAccess<TAddress, TData>
+	{
+		void Poke(TAddress address, TData value);
+		TData Peek(TAddress address);
+	}
+}
---
+++ src/Core/KillerApps.Emulation.Core8Bit/ProcessorBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KillerApps.Emulation.Core
+{
+	public abstract class ProcessorBase
+	{
+		public abstract void Initialize();
+		public abstract void Reset();
+		public abstract ulong Execute(int instructionsToExecute);
+		public abstract object SignalInterrupt(InterruptType interrupt, params object[] details);
+	}
+}
